349b613e87db2c2f862ffd595afca03199f28fae
vishesh-baj <vishesh.bajpayee@icloud.com>
2023-11-18 21:06:22 +0530

    added classes

--- todo_app/assets/scripts/app.js
+++ todo_app/assets/scripts/app.js
@@ -1,41 +1,58 @@
-const todoInput = document.getElementById("todo_input");
-const addTodoButton = document.querySelector("section form button");
-const todoContainer = document.querySelector("aside section");
-const allTodos = [];
+class TodoManager {
+  constructor() {
+    this.todoInput = document.getElementById("todo_input");
+    this.addTodoButton = document.querySelector("section form button");
+    this.todoContainer = document.querySelector("aside section");
+    this.allTodos = [];
 
-const deleteTodo = (id) => {
-  // Remove the todo from the allTodos array
-  const updatedTodos = allTodos.filter((todo) => todo.id !== id);
-  allTodos.length = 0;
-  allTodos.push(...updatedTodos);
-
-  // Remove the todo element from the DOM
-  const todoElement = document.getElementById(id);
-  todoElement.remove();
-};
-
-addTodoButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const todoObj = {
-    id: Math.random(),
-    title: todoInput.value,
-  };
-  allTodos.push(todoObj);
-  const todoDiv = document.createElement("div");
-  todoDiv.id = todoObj.id;
-  todoDiv.innerHTML = `
-    <h2>${todoObj.title}</h2>
-    <button  class="delete_todo">delete</button>
-    <button class="edit_todo">edit</button>
-    `;
-  todoContainer.append(todoDiv);
-  todoInput.value = "";
-  console.log("Clicked on add todo button", todoInput.value, allTodos);
-});
-
-todoContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("delete_todo")) {
-    deleteTodo(e.target.parentElement.id);
+    this.addTodoButton.addEventListener("click", this.handleAddTodo.bind(this));
+    this.todoContainer.addEventListener(
+      "click",
+      this.handleTodoContainerClick.bind(this)
+    );
   }
-});
 
+  handleAddTodo(e) {
+    e.preventDefault();
+    const todoObj = {
+      id: Math.random(),
+      title: this.todoInput.value,
+    };
+    this.allTodos.push(todoObj);
+    this.renderTodo(todoObj);
+    this.todoInput.value = "";
+    console.log(
+      "Clicked on add todo button",
+      this.todoInput.value,
+      this.allTodos
+    );
+  }
+
+  renderTodo(todoObj) {
+    const todoDiv = document.createElement("div");
+    todoDiv.id = todoObj.id;
+    todoDiv.innerHTML = `
+      <h2>${todoObj.title}</h2>
+      <button class="delete_todo">delete</button>
+      <button class="edit_todo">edit</button>
+    `;
+    this.todoContainer.append(todoDiv);
+  }
+
+  handleTodoContainerClick(e) {
+    if (e.target.classList.contains("delete_todo")) {
+      this.deleteTodo(e.target.parentElement.id);
+    }
+  }
+
+  deleteTodo(id) {
+    const updatedTodos = this.allTodos.filter((todo) => todo.id !== id);
+    this.allTodos.length = 0;
+    this.allTodos.push(...updatedTodos);
+
+    const todoElement = document.getElementById(id);
+    todoElement.remove();
+  }
+}
+
+const todoManager = new TodoManager();
